70b2fc292e47fd126ca1c1a4d17b9de7236050b8
carlDguez19 <carldominguez19@gmail.com>
2024-09-08 16:26:20 -0600

    did prettyPrint modified to be a function withing the BST class. I will now work on the insert and delete funcitons

--- src/BST.js
+++ src/BST.js
@@ -6,4 +6,29 @@ export class BST {
         this.arrData = arr;
         this.root = this.buildTree(this.arrData);
     }
+
+    buildTree(arr, start, end) {
+        if (start > end) return null;
+        let mid = (start + end) / 2;
+        let root = new Node(arr[mid]);
+
+        root.left = this.buildTree(arr, 0, mid);
+        root.right = this.buildTree(arr, mid + 1, end);
+
+        return root;
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    };
+
 }
